1c3d02e5b88f6ee483e9288d8b85f6a89173c22f
Anirudh Kumar Subramanyam <ask710@uw.edu>
2017-02-02 14:51:05 -0800

    Add partially completed files

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -10,11 +10,11 @@ api.key <- nty.apikey
 # Define a function that takes in the name of a movie as an argument and returns
 # a list of information about that movie. The steps for this algorithm are below:
 MovieInfo <- function(movie){
-}
+
 # Construct an HTTP request to search for reviews for the given movie.
  base.uri <- "https://api.nytimes.com/svc/movies/v2/"
  resource <- "reviews/search.json"
- query.params <- list("api-key"=api.key, query = "Shawshank")
+ query.params <- list("api-key"=api.key, query = "movie")
 # See the interactive console for more detail:
 #   https://developer.nytimes.com/movie_reviews_v2.json#/Console/GET/reviews/search.json
 # You should use YOUR api key (as the `api-key` parameter)
@@ -23,7 +23,7 @@ body <- fromJSON(content(response, "text"))
 View(body)
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+}
 
 # What kind of data structure did this return?
 
@@ -41,3 +41,4 @@ View(body)
 
 
 # Test that your function works with a movie of your choice
+MovieInfo("Shawshank")
